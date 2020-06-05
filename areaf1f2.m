d1aae51ce8eb4f5a6ea3148f2443761a0e71e201
piotrkasprzakMT <66119650+piotrkasprzakMT@users.noreply.github.com>
2020-06-05 14:21:13 +0200

    Create areaf1f2.m

---
+++ areaf1f2.m
@@ -0,0 +1,51 @@
+f1 = @(x)   2 + 1 ./ (1 + x.^2);
+f2 = @(x)   2 + sin(x);
+
+a = 0;
+b = 5;
+N = 1000;
+
+x = linspace(a, b, N);
+
+figure(1);
+clf;
+plot(x, f1(x));
+grid on;
+grid minor;
+hold on;
+plot(x, f2(x));
+
+f21 = @(x) f2(x) - f1(x);
+
+for i = 1:(N-1)
+    try
+        xL = fzero(f21, [x(i), x(i+1)]);
+        break
+    catch
+    end
+end
+
+for i = N:-1:2
+    try
+        xR = fzero(f21, [x(i-1), x(i)]);
+        break
+    catch
+    end
+end
+
+xLR = linspace(xL, xR, N);
+xRL = flip(xLR);
+
+yLR = f2(xLR);
+yRL = f1(xRL);
+
+xi = [xLR, xRL];
+yi = [yLR, yRL];
+
+fill(xi, yi, 'y');
+
+text(xL,2.4,'\uparrow x=0.70; y=2.66 ')
+text(xR,f1(xR),'\leftarrow x=3.04; y=2.10')
+
+format long;
+quadgk(f21, xL, xR)
